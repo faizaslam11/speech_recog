b852d92033f98868592b3126b5e0a1003cc58118
Faiz Aslam <60103930+faizaslam11@users.noreply.github.com>
2021-03-15 23:33:16 +0530

    Update voicerecog.py

--- voicerecog.py
+++ voicerecog.py
@@ -16,7 +16,7 @@ with sr.Microphone() as source:
     audio= r.listen(source)
     try:
         text = r.recognize_google(audio)
-        print("you said : {}".format(text))
+            print("you said : {}".format(text))
         url = 'https://www.google.co.in/search?q='
         search_url= url+text
         webbrowser.open(search_url)
